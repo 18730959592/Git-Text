45156bfab0638286d7314f50b871828791b40d98
lanou3g <154767005@qq.com>
2016-05-11 19:44:47 +0800

    添加了

---
+++ GirText/GirText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GirText
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 王闯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GirText/GirText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GirText
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 王闯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
